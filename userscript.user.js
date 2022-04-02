2dabbd5e7625871c245f40e2edd805aa81f82385
Jason Huang <jasonhuang20035@gmail.com>
2022-04-02 13:36:45 -0400

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         McMaster Logo template
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the canvas!
 // @author       oralekin
 // @match        https://hot-potato.reddit.com/embed*
