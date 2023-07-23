ec33a71967f6686becaa51c336893e119dcacc5a
Jugal Bilimoria <jugalbili@yahoo.ca>
2023-07-23 14:40:24 -0400

    Updated overlay script

--- 2023/userscript.user.js
+++ 2023/userscript.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         McMaster + Friendly Unis Logo template
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      2023.1
 // @description  try to take over the canvas!
-// @author       oralekin
+// @author       oralekin and xpert104
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://github.com/err53/mac-place-template/blob/main/2023/userscript.user.js
+// @downloadURL  https://github.com/err53/mac-place-template/blob/main/2023/userscript.user.js
 // @grant        none
 // ==/UserScript==
 // Shamelessly stolen from the osu! r/place team (thanks!)
@@ -14,19 +16,19 @@ if (window.top !== window.self) {
     window.addEventListener('load', () => {
         // Load the image
         const image = document.createElement("img");
-        image.src = "https://github.com/err53/mac-place-template/raw/main/dotted-place-template-mac.png";
+        image.src = "https://raw.githubusercontent.com/err53/mac-place-template/main/2023/dotted-place-template-mac.png";
         image.onload = () => {
             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
         };
       
         // Add the image as overlay
-        const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
-        const canvas = camera.querySelector("mona-lisa-canvas");
+        const camera = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("garlic-bread-camera");
+        const canvas = camera.querySelector("garlic-bread-canvas");
         canvas.shadowRoot.querySelector('.container').appendChild(image);
       
         // Add a style to put a hole in the pixel preview (to see the current or desired color)
         const waitForPreview = setInterval(() => {
-            const preview = camera.querySelector("mona-lisa-pixel-preview");
+            const preview = camera.querySelector("garlic-bread-pixel-preview");
             if (preview) {
               clearInterval(waitForPreview);
               const style = document.createElement('style')
